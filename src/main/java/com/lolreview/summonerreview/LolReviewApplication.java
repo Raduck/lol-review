ec2d7833c3d6613f6a9629283e24c3eceb6ff58e
DESKTOP-A0IKVP0\USER <lhyunjava@naver.com>
2025-07-16 10:28:12 +0900

    Project Add

---
+++ src/main/java/com/lolreview/summonerreview/LolReviewApplication.java
@@ -0,0 +1,13 @@
+package com.lolreview.summonerreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolReviewApplication.class, args);
+	}
+
+}
